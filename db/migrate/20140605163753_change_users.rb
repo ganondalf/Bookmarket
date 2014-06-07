c410b091b005bea11c9fa2151af727c19cda6b53
ganondalf <cganon@gmail.com>
2014-06-06 23:53:39 -0400

    add first name and last name to login and edit

--- db/migrate/20140605163753_change_users.rb
+++ db/migrate/20140605163753_change_users.rb
@@ -3,7 +3,8 @@ class ChangeUsers < ActiveRecord::Migration
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :picture, :text
-    add_column :users, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
     add_column :users, :bookmark_token, :string
   end
 end
